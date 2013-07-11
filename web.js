5a22c8e76c05180ecd463de259f41c55df4feab2
ElFuturero <hector.e.meza@gmail.com>
2013-07-11 18:36:05 +0000

    Modified web.js to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,13 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
+var indexPage = "index.html";
+var bufferIndex = fs.readFileSync(indexPage);
+var message = buf.toString(bufferIndex, 'utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-});
+  response.send(message);
+}
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
